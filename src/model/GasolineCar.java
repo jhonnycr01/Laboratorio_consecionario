b8a6b5c156e96e339199ccb3d3758b6a8ad06ee3
jhonnycr01 <54719958+jhonnycr01@users.noreply.github.com>
2020-05-07 19:52:24 -0500

    created Class enum GasType and implements constructor getters and setter

--- src/model/GasolineCar.java
+++ src/model/GasolineCar.java
@@ -1,9 +1,37 @@
 package model;
 
-public class GasolineCar {
+public class GasolineCar extends Car {
 
-	public GasolineCar() {
-		// TODO Auto-generated constructor stub
+	private int tankCapacity;
+	private GasType gasType;
+	private int gasConsume;
+	public GasolineCar(double totalSalePrice, double basePrice, String brand, String model, VehicleState vehicleState,
+			String plate, float displacement, String soat, String tecno, Client owner, CarType carType, int numberDoors,
+			boolean polarized, int tankCapacity, GasType gasType, int gasConsume) {
+		super(totalSalePrice, basePrice, brand, model, vehicleState, plate, displacement, soat, tecno, owner, carType,
+				numberDoors, polarized);
+		this.tankCapacity = tankCapacity;
+		this.gasType = gasType;
+		this.gasConsume = gasConsume;
 	}
-
+	public int getTankCapacity() {
+		return tankCapacity;
+	}
+	public void setTankCapacity(int tankCapacity) {
+		this.tankCapacity = tankCapacity;
+	}
+	public GasType getGasType() {
+		return gasType;
+	}
+	public void setGasType(GasType gasType) {
+		this.gasType = gasType;
+	}
+	public int getGasConsume() {
+		return gasConsume;
+	}
+	public void setGasConsume(int gasConsume) {
+		this.gasConsume = gasConsume;
+	}
+	
+	
 }
